6bfa1ff7e1c35986ac17e4696552a3ff36127c29
Kirill Bulgakov <kbulgakovdev@gmail.com>
2018-08-10 00:14:49 +0700

    docs: Add comments for UseCaseDuration

--- domain/src/main/java/com/and/movies/domain/usecase/base/UseCaseDuration.java
+++ domain/src/main/java/com/and/movies/domain/usecase/base/UseCaseDuration.java
@@ -8,6 +8,9 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.SOURCE)
 @IntDef({UseCaseDuration.SHORT_RUNNING, UseCaseDuration.LONG_RUNNING})
 public @interface UseCaseDuration {
+    //like small computations or disk reading
     int SHORT_RUNNING = 0;
+
+    //like network request
     int LONG_RUNNING = 1;
 }
