7393726eedcb31b3b4f6c96c3a529d6390de5e15
Kirill Bulgakov <kbulgakovdev@gmail.com>
2018-08-10 00:13:08 +0700

    feat: Add usecase for getting movies list

---
+++ domain/src/main/java/com/and/movies/domain/usecase/GetMovieListUseCase.java
@@ -0,0 +1,59 @@
+package com.and.movies.domain.usecase;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
+
+import com.and.movies.domain.repo.error.RepoException;
+import com.and.movies.domain.repo.movie.MovieInfo;
+import com.and.movies.domain.repo.movie.MovieRepo;
+import com.and.movies.domain.repo.movie.MoviesSortOrder;
+import com.and.movies.domain.usecase.base.BaseUseCase;
+import com.and.movies.domain.usecase.base.UseCaseDuration;
+import com.and.movies.domain.usecase.resource.Resource;
+import com.and.movies.domain.usecase.resource.ResponseListener;
+import com.and.movies.domain.usecase.scheduler.UseCaseScheduler;
+import com.google.auto.value.AutoValue;
+
+import java.util.List;
+
+public class GetMovieListUseCase extends BaseUseCase<GetMovieListUseCase.Request, List<MovieInfo>> {
+    @NonNull
+    private final MovieRepo mMovieRepo;
+
+    protected GetMovieListUseCase(@NonNull final UseCaseScheduler useCaseScheduler,
+                                  @NonNull final MovieRepo movieRepo) {
+        super(useCaseScheduler, UseCaseDuration.LONG_RUNNING);
+        mMovieRepo = movieRepo;
+    }
+
+    @Override
+    @WorkerThread
+    public void onExecute(@Nullable final Request request,
+                          @NonNull final ResponseListener<List<MovieInfo>> responseListener) {
+        if (request == null) {
+            responseListener.onResponse(Resource.failed(new IllegalArgumentException("Request is null")));
+            return;
+        }
+        responseListener.onResponse(Resource.inProgress());
+
+        try {
+            final List<MovieInfo> movies = mMovieRepo.getMovies(request.getSortOrder());
+            responseListener.onResponse(Resource.succeed(movies));
+        } catch (RepoException e) {
+            responseListener.onResponse(Resource.failed(e));
+        }
+    }
+
+
+    @AutoValue
+    public abstract static class Request {
+        static Request create(@MoviesSortOrder final int sortOrder) {
+            return new AutoValue_GetMovieListUseCase_Request(sortOrder);
+        }
+
+        @MoviesSortOrder
+        abstract int getSortOrder();
+
+    }
+}
